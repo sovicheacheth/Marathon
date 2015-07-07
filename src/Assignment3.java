ab95be30ae377d1bcb1aa024c2989613c99845a6
MichaelRuffe <Michaelruffe@gmail.com>
2015-07-07 13:52:39 -0400

    Code for fastest runner created.

--- src/Assignment3.java
+++ src/Assignment3.java
@@ -1,6 +1,20 @@
 class Marathon {
 	
+	public static int fastestTime(int[] time){
+		int runTime = time[0];
+		int location = 0;
+		for(int i=0; i<time.length;i++){
+			if(time[i]<runTime){
+				runTime = time[i];
+				location = i;
+			}
+		}
+		return location;
+	}
+	
 	public static void main (String[] arguments) {
+		
+		int timeLocation;
 		
 		//String array of runners
 		String[] names = {
@@ -13,8 +27,12 @@ class Marathon {
 						334, 412, 393, 299, 343, 317, 265 };
 		
 		//Loop to combine names with their times
+		System.out.println("List of Runners:");
 		for (int i = 0; i < names.length; i++) {
 				System.out.println(names[i] + ": " + times[i]);
 		}
+		System.out.println("\nThe fastest runner and time was:");
+		timeLocation=fastestTime(times);
+		System.out.println(names[timeLocation]+ ": " + times[timeLocation]);
 	}
 }
