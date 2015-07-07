d58204bdf856f4f22cd1cd7d2c339ee61a19697f
Michael_Ruffe <Michael_Ruffe@michaeluffesmbp.home>
2015-07-07 14:08:09 -0400

    Second Place code implemented. Completion of Assignment.

--- src/Assignment3.java
+++ src/Assignment3.java
@@ -1,8 +1,12 @@
 class Marathon {
 	
 	public static int fastestTime(int[] time){
+		//Make the first runner the fastest time.
 		int runTime = time[0];
 		int location = 0;
+		
+		//Compare runner, if faster make that lowest.
+		//If not do nothing.
 		for(int i=0; i<time.length;i++){
 			if(time[i]<runTime){
 				runTime = time[i];
@@ -11,10 +15,37 @@ class Marathon {
 		}
 		return location;
 	}
+	public static int secondFastest(int[] time, int location){
+		int runTime;
+		int secondLocation;
+		//If the fastest runner is first spot of array, start with second
+		if(location==0){
+			runTime = time[1];
+			secondLocation = 1;
+		}
+		else{
+			runTime = time[0];
+			secondLocation = 0;
+		}
+		//Compare runner, if faster make that lowest.
+		//If not do nothing.
+		for(int i=0; i<time.length;i++){
+			//When we get to fastest runner, bypass
+			if(i==location)
+				continue;
+			if(time[i]<runTime){
+				runTime = time[i];
+				secondLocation = i;
+			}
+		}
+		
+		return secondLocation;
+	}
 	
 	public static void main (String[] arguments) {
 		
-		int timeLocation;
+		int firstTimeLocation;
+		int secondTimeLocation;
 		
 		//String array of runners
 		String[] names = {
@@ -31,8 +62,12 @@ class Marathon {
 		for (int i = 0; i < names.length; i++) {
 				System.out.println(names[i] + ": " + times[i]);
 		}
+		//Method call and outputs
 		System.out.println("\nThe fastest runner and time was:");
-		timeLocation=fastestTime(times);
-		System.out.println(names[timeLocation]+ ": " + times[timeLocation]);
+		firstTimeLocation=fastestTime(times);
+		System.out.println(names[firstTimeLocation]+ ": " + times[firstTimeLocation]);
+		System.out.println("\nThe second fastest runner and time was:");
+		secondTimeLocation= secondFastest(times,firstTimeLocation);
+		System.out.println(names[secondTimeLocation]+ ": " + times[secondTimeLocation]);
 	}
 }
